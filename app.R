0952336e6c414ed529746d39ec0b391bc22c89fc
gamaly <gamaly@gmail.com>
2019-07-23 15:53:58 -0400

    Added map to the front page. Small changes to the graphs. Started to make use of rows and columns on the overview page.

--- app.R
+++ app.R
@@ -4,29 +4,45 @@ library(jsonlite)
 #library(ggplot2)
 library(plotly)
 library(RColorBrewer)
+library(leaflet)
+library(dplyr)
+library(lubridate)
 
-# IMPORT ACLED DATA. DON"T USE THIS AT THE BEGINNING IN PRODUCTION
+#IMPORT ACLED DATA. DON"T USE THIS AT THE BEGINNING IN PRODUCTION
 # ACLED_API_Query <- stream_in(file("https://api.acleddata.com/acled/read?country=myanmar&limit=1000000&terms=accept"))
 # ACLED_Data <- as.data.frame(ACLED_API_Query$data)
 # ACLED_Data$event_date <- as.Date(ACLED_Data$event_date)
 # ACLED_Data$fatalities <- as.integer(ACLED_Data$fatalities)
+# ACLED_Data$longitude <- as.double(ACLED_Data$longitude)
+# ACLED_Data$latitude <- as.double(ACLED_Data$latitude)
+#ACLED_Data$data_id <- as.integer(ACLED_Data$data_id)
+#save(ACLED_Data, file = "data/ACLED_Data.rdata")
 
-
-
+load(file = "data/ACLED_Data.rdata")
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("Conflict Events Dashboard", theme = shinytheme("paper"),
+ui <- navbarPage("Conflict Events Report", theme = shinytheme("paper"),
                            tabPanel("Overview",
-                                    h2("Country Overview"),
-                                    plotlyOutput("eventsPerYear"),
-                                    plotlyOutput("fatalitiesPerYear")
+                                    fluidPage(
+                                    h4("Country Overview"),
+                                    fluidRow(
+                                      column(9,
+                                    leafletOutput("overviewMap"),
+                                    plotlyOutput("eventsPerMonth", height = 200)))
+                                    )
+
+                                    
                                     ),
                            navbarMenu("Regional Snapshots",
                                       tabPanel("Shan"),
                                       tabPanel("Rakhine"),
                                       tabPanel("Kachin")
                                        ),
-                           tabPanel("Events over Time"),
+                           tabPanel("Events over Time",
+                                    h2("Total State Level Events Per Year"),
+                                    plotlyOutput("eventsPerYear"),
+                                    plotlyOutput("fatalitiesPerYear")
+                                    ),
                            tabPanel("Key Actors"),
                            tabPanel("About",
                                     h1("About"),
@@ -38,21 +54,37 @@ ui <- navbarPage("Conflict Events Dashboard", theme = shinytheme("paper"),
                            )
                 
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
-
-  cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  ######### OVERVIEW PAGE
+  tcu_map <- "https://api.mapbox.com/styles/v1/gamaly/cjmhaei90a3xh2sp6lfqftcqg/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2FtYWx5IiwiYSI6ImNpZmswdTM3bGN2eXFzNG03OTd6YWZhNmEifQ.srtQMx2-zlTgvAT90pAOTw"
+  map_attr <- "© <a href='https://www.mapbox.com/map-feedback/'>Mapbox</a>"
   
+  #Leaflet Map
+  output$overviewMap <- renderLeaflet({
+    leaflet(data = ACLED_Data) %>% addTiles(urlTemplate = tcu_map, attribution = map_attr)  %>% addCircleMarkers(~longitude, ~latitude, radius = sqrt(ACLED_Data$fatalities))})
+  
+  #Events Over Time
+  Monthly <- ACLED_Data %>% group_by(month=floor_date(event_date, "month")) %>% summarise(total = length(month))
+  
+  monthlyEvents <- ggplot(Monthly, aes(month, total)) + geom_line() + ggtitle("Total events per month over time") + theme_classic()
+  
+  output$eventsPerMonth <- renderPlotly({monthlyEvents})
+  
+  
+  ####### STATE LEVEL EVENTS PER YEAR PAGE
   
   # fatalities_year <- aggregate(ACLED_Data['fatalities'], by=list(ACLED_Data['admin1'], ACLED_Data['year']), sum)
-  
-  eventsYear <-  ggplot(ACLED_Data, aes(year, fill=admin1)) + geom_bar() + ggtitle("Events Per Year") + theme(plot.title = element_text(size=14, face="bold"))
+  eventsYear <-  ggplot(ACLED_Data, aes(year, fill=admin1)) + geom_bar() + ggtitle("Events Per Year") + theme(plot.title = element_text(size=14, face="bold")) + theme_classic()
   
    output$eventsPerYear <- renderPlotly({ggplotly(eventsYear)})
    
-   fatalitiesYear <- ggplot(ACLED_Data, aes(x=year, y=fatalities, fill=admin1)) + geom_bar(stat="identity") + ggtitle("Fatalities Per Year") + theme(plot.title = element_text(size=14, face="bold"))
+   fatalitiesYear <- ggplot(ACLED_Data, aes(x=year, y=fatalities, fill=admin1)) + geom_bar(stat="identity") + ggtitle("Fatalities Per Year") + theme(plot.title = element_text(size=14, face="bold")) + theme_classic()
    
    output$fatalitiesPerYear <- renderPlotly({ggplotly(fatalitiesYear)})
+   
+   
+   
    
 }
 
