d1e786620e38b3a7da17c0c816861b965552580c
gamaly <gamaly@gmail.com>
2019-07-23 20:31:17 -0400

    Added new bar chart to overview page. Chart shows total of Admin 1.

--- app.R
+++ app.R
@@ -15,78 +15,120 @@ library(lubridate)
 # ACLED_Data$fatalities <- as.integer(ACLED_Data$fatalities)
 # ACLED_Data$longitude <- as.double(ACLED_Data$longitude)
 # ACLED_Data$latitude <- as.double(ACLED_Data$latitude)
-#ACLED_Data$data_id <- as.integer(ACLED_Data$data_id)
-#save(ACLED_Data, file = "data/ACLED_Data.rdata")
+# ACLED_Data$data_id <- as.integer(ACLED_Data$data_id)
+# save(ACLED_Data, file = "data/ACLED_Data.rdata")
 
 load(file = "data/ACLED_Data.rdata")
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("Conflict Events Report", theme = shinytheme("paper"),
-                           tabPanel("Overview",
-                                    fluidPage(
-                                    h4("Country Overview"),
-                                    fluidRow(
-                                      column(9,
-                                    leafletOutput("overviewMap"),
-                                    plotlyOutput("eventsPerMonth", height = 200)))
-                                    )
-
-                                    
-                                    ),
-                           navbarMenu("Regional Snapshots",
-                                      tabPanel("Shan"),
-                                      tabPanel("Rakhine"),
-                                      tabPanel("Kachin")
-                                       ),
-                           tabPanel("Events over Time",
-                                    h2("Total State Level Events Per Year"),
-                                    plotlyOutput("eventsPerYear"),
-                                    plotlyOutput("fatalitiesPerYear")
-                                    ),
-                           tabPanel("Key Actors"),
-                           tabPanel("About",
-                                    h1("About"),
-                                    p("This dashboard shows Myanmar conflict event data produced by the Armed Conflict Location and Event Dataset (ACLED). Data is generally updated weekly. Read about the Data Methodology to learn more about the dataset."),
-                                    p("Source: Armed Conflict Location & Event Project (ACLED) Codebook, 2017, Version 8")
-                                    
-                                    )
+ui <-
+  navbarPage(
+    "Conflict Events Report",
+    theme = shinytheme("paper"),
+    tabPanel("Overview",
+             fluidPage(
+               h4("Country Overview"),
+               fluidRow(
+                 column(
+                   8,
+                   leafletOutput("overviewMap"),
+                   plotlyOutput("eventsPerMonth", height = 200)
+                 ),
+                 column(4,
+                        plotlyOutput("eventsPerAdmin1"))
                  
-                           )
-                
+               )
+             )),
+    navbarMenu(
+      "Regional Snapshots",
+      tabPanel("Shan"),
+      tabPanel("Rakhine"),
+      tabPanel("Kachin")
+    ),
+    tabPanel(
+      "Events over Time",
+      h2("Total State Level Events Per Year"),
+      plotlyOutput("eventsPerYear"),
+      plotlyOutput("fatalitiesPerYear")
+    ),
+    tabPanel("Key Actors"),
+    tabPanel(
+      "About",
+      h1("About"),
+      p(
+        "This dashboard shows Myanmar conflict event data produced by the Armed Conflict Location and Event Dataset (ACLED). Data is generally updated weekly. Read about the Data Methodology to learn more about the dataset."
+      ),
+      p(
+        "Source: Armed Conflict Location & Event Project (ACLED) Codebook, 2017, Version 8"
+      )
+      
+    )
+    
+  )
+
 
 # Define server logic
 server <- function(input, output) {
   ######### OVERVIEW PAGE
-  tcu_map <- "https://api.mapbox.com/styles/v1/gamaly/cjmhaei90a3xh2sp6lfqftcqg/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2FtYWx5IiwiYSI6ImNpZmswdTM3bGN2eXFzNG03OTd6YWZhNmEifQ.srtQMx2-zlTgvAT90pAOTw"
-  map_attr <- "© <a href='https://www.mapbox.com/map-feedback/'>Mapbox</a>"
+  tcu_map <-
+    "https://api.mapbox.com/styles/v1/gamaly/cjmhaei90a3xh2sp6lfqftcqg/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2FtYWx5IiwiYSI6ImNpZmswdTM3bGN2eXFzNG03OTd6YWZhNmEifQ.srtQMx2-zlTgvAT90pAOTw"
+  map_attr <-
+    "© <a href='https://www.mapbox.com/map-feedback/'>Mapbox</a>"
   
   #Leaflet Map
   output$overviewMap <- renderLeaflet({
-    leaflet(data = ACLED_Data) %>% addTiles(urlTemplate = tcu_map, attribution = map_attr)  %>% addCircleMarkers(~longitude, ~latitude, radius = sqrt(ACLED_Data$fatalities))})
+    leaflet(data = ACLED_Data) %>% addTiles(urlTemplate = tcu_map, attribution = map_attr)  %>% addCircleMarkers( ~
+                                                                                                                    longitude, ~ latitude, radius = sqrt(ACLED_Data$fatalities))
+  })
   
   #Events Over Time
-  Monthly <- ACLED_Data %>% group_by(month=floor_date(event_date, "month")) %>% summarise(total = length(month))
+  Monthly <-
+    ACLED_Data %>% group_by(month = floor_date(event_date, "month")) %>% summarise(total = length(month))
   
-  monthlyEvents <- ggplot(Monthly, aes(month, total)) + geom_line() + ggtitle("Total events per month over time") + theme_classic()
+  ACLED_Data %>% group_by(month = floor_date(event_date, "month")) %>% summarise(total = length(month))
   
-  output$eventsPerMonth <- renderPlotly({monthlyEvents})
+  monthlyEvents <-
+    ggplot(Monthly, aes(month, total)) + geom_line(color = "darkblue") + ggtitle("Total events per month over time") + theme_classic()
+  
+  output$eventsPerMonth <- renderPlotly({
+    monthlyEvents
+  })
+  
+  
+  totalAdmin1 <-
+    ggplot(ACLED_Data, aes(admin1)) + geom_bar(fill = "darkblue") + theme_classic() + coord_flip() + ggtitle("Total Events Per State")
+  
+  output$eventsPerAdmin1 <- renderPlotly({
+    totalAdmin1
+  })
   
   
   ####### STATE LEVEL EVENTS PER YEAR PAGE
   
   # fatalities_year <- aggregate(ACLED_Data['fatalities'], by=list(ACLED_Data['admin1'], ACLED_Data['year']), sum)
-  eventsYear <-  ggplot(ACLED_Data, aes(year, fill=admin1)) + geom_bar() + ggtitle("Events Per Year") + theme(plot.title = element_text(size=14, face="bold")) + theme_classic()
+  eventsYear <-
+    ggplot(ACLED_Data, aes(year, fill = admin1)) + geom_bar() + ggtitle("Events Per Year") + theme(plot.title = element_text(size =
+                                                                                                                               14, face = "bold")) + theme_classic()
   
-   output$eventsPerYear <- renderPlotly({ggplotly(eventsYear)})
-   
-   fatalitiesYear <- ggplot(ACLED_Data, aes(x=year, y=fatalities, fill=admin1)) + geom_bar(stat="identity") + ggtitle("Fatalities Per Year") + theme(plot.title = element_text(size=14, face="bold")) + theme_classic()
-   
-   output$fatalitiesPerYear <- renderPlotly({ggplotly(fatalitiesYear)})
-   
-   
-   
-   
+  output$eventsPerYear <- renderPlotly({
+    ggplotly(eventsYear)
+  })
+  
+  fatalitiesYear <-
+    ggplot(ACLED_Data, aes(x = year, y = fatalities, fill = admin1)) + geom_bar(stat =
+                                                                                  "identity") + ggtitle("Fatalities Per Year") + theme(plot.title = element_text(size =
+                                                                                                                                                                   14, face = "bold")) + theme_classic()
+  
+  output$fatalitiesPerYear <-
+    renderPlotly({
+      ggplotly(fatalitiesYear)
+    })
+  
+  
+  
+  
+  
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
